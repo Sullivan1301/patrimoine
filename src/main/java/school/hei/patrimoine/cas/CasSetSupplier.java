29228837973bc1657797d7aff0e907533581a217
Sullivan Joro <119728329+Sullivan1301@users.noreply.github.com>
2025-04-08 09:40:25 +0300

    feat: implement CasSetSupplier to return a new CasSet instance

--- src/main/java/school/hei/patrimoine/cas/CasSetSupplier.java
+++ src/main/java/school/hei/patrimoine/cas/CasSetSupplier.java
@@ -1,10 +1,17 @@
+
 package school.hei.patrimoine.cas;
 
+import school.hei.patrimoine.cas.pro3.BakoCas;
+import school.hei.patrimoine.modele.Argent;
+import school.hei.patrimoine.modele.Devise;
+
+import java.util.Set;
 import java.util.function.Supplier;
 
 public class CasSetSupplier implements Supplier<CasSet> {
   @Override
   public CasSet get() {
-    throw new RuntimeException("Implement it");
+    Argent deviseAsArgent = new Argent(0, Devise.MGA);
+    return new CasSet(Set.of(new BakoCas()), deviseAsArgent);
   }
 }
